cfdbe2418922da137cab008f2faa660b788bc619
Antoine Jacquey <antoine.jacquey@gmail.com>
2017-07-28 10:55:46 +0200

    Initial commit for the Golem simulator.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 Golem
 =====
 
-"Fork Golem" to create a new MOOSE-based application.
+GOLEM is a numerical simulator for modelling coupled Thermo-Hydro-Mechanical processes in faulted geothermal reservoirs.
+The simulator is developed by Antoine Jacquey and Mauro Cacace at the [GFZ German Research Centre for Geosciences](http://www.gfz-potsdam.de).
+GOLEM is a MOOSE-based application. Visit the [MOOSE framework](http://mooseframework.org) page for more information.
 
-For more information see: [http://mooseframework.org/create-an-app/](http://mooseframework.org/create-an-app/)
+GOLEM is distributed under the [GNU LESSER GENERAL PUBLIC LICENSE](https://github.com/ajacquey/Golem/LICENSE).
+
+If you use GOLEM for your work please cite:
+Cacace, M. and Jacquey, A. B.: Flexible parallel implicit modelling of coupled Thermal-Hydraulic-Mechanical processes in fractured rocks, Solid Earth Discuss., https://doi.org/10.5194/se-2017-33, in review, 2017.
--- unit/src/main.C
+++ unit/src/main.C
@@ -1,16 +1,22 @@
-/****************************************************************/
-/*               DO NOT MODIFY THIS HEADER                      */
-/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
-/*                                                              */
-/*           (c) 2010 Battelle Energy Alliance, LLC             */
-/*                   ALL RIGHTS RESERVED                        */
-/*                                                              */
-/*          Prepared by Battelle Energy Alliance, LLC           */
-/*            Under Contract No. DE-AC07-05ID14517              */
-/*            With the U. S. Department of Energy               */
-/*                                                              */
-/*            See COPYRIGHT for full restrictions               */
-/****************************************************************/
+/******************************************************************************/
+/*           GOLEM - Multiphysics of faulted geothermal reservoirs            */
+/*                                                                            */
+/*          Copyright (C) 2017 by Antoine B. Jacquey and Mauro Cacace         */
+/*             GFZ Potsdam, German Research Centre for Geosciences            */
+/*                                                                            */
+/*    This program is free software: you can redistribute it and/or modify    */
+/*    it under the terms of the GNU General Public License as published by    */
+/*      the Free Software Foundation, either version 3 of the License, or     */
+/*                     (at your option) any later version.                    */
+/*                                                                            */
+/*       This program is distributed in the hope that it will be useful,      */
+/*       but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/*        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       */
+/*                GNU General Public License for more details.                */
+/*                                                                            */
+/*      You should have received a copy of the GNU General Public License     */
+/*    along with this program.  If not, see <http://www.gnu.org/licenses/>    */
+/******************************************************************************/
 
 #include "GolemApp.h"
 #include "gtest/gtest.h"
