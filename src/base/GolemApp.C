4d2b3f83d1678ba42ed20fb97437beefd79b4878
Antoine Jacquey <ajacquey@users.noreply.github.com>
2018-10-11 13:37:02 +0200

    Corrected registration with Action.

--- src/base/GolemApp.C
+++ src/base/GolemApp.C
@@ -24,75 +24,6 @@
 #include "ModulesApp.h"
 #include "MooseSyntax.h"
 
-// Actions
-#include "GolemPressureAction.h"
-
-// Materials
-#include "GMSMaterial.h"
-#include "GolemMaterialBase.h"
-#include "GolemMaterialH.h"
-#include "GolemMaterialT.h"
-#include "GolemMaterialTH.h"
-#include "GolemMaterialMElastic.h"
-#include "GolemMaterialMInelastic.h"
-#include "GolemDruckerPrager.h"
-
-// Kernels
-#include "GMSEnergyTimeDerivative.h"
-#include "GMSEnergyResidual.h"
-#include "GMSMassResidual.h"
-#include "GolemKernelTimeH.h"
-#include "GolemKernelTimeT.h"
-#include "GolemKernelH.h"
-#include "GolemKernelT.h"
-#include "GolemKernelTH.h"
-#include "GolemKernelM.h"
-#include "GolemKernelHPoroElastic.h"
-
-// AuxKernels
-#include "GolemDarcyVelocity.h"
-#include "GolemFluidVelocity.h"
-#include "GolemStress.h"
-#include "GolemStrain.h"
-#include "GolemThermalStress.h"
-#include "GolemEqvInelasticStrain.h"
-
-// DiracKernels
-#include "GolemDiracKernelTH.h"
-
-// BCs
-#include "GolemConvectiveTHBC.h"
-#include "GolemVelocityBC.h"
-#include "GolemHeatFlowBC.h"
-#include "GolemPressureBC.h"
-
-// Controls
-#include "GolemTimeControl.h"
-
-// Functions
-#include "GolemFunctionBCFromFile.h"
-#include "GolemFunctionReadFile.h"
-
-// UserObjects
-#include "GolemScaling.h"
-#include "GolemSUPG.h"
-#include "GolemPropertyReadFile.h"
-#include "GolemFluidDensityConstant.h"
-#include "GolemFluidDensityLinear.h"
-#include "GolemFluidDensityIAPWS.h"
-#include "GolemFluidViscosityConstant.h"
-#include "GolemFluidViscosityLinear.h"
-#include "GolemFluidViscosityIAPWS.h"
-#include "GolemPorosityConstant.h"
-#include "GolemPorosityTHM.h"
-#include "GolemPermeabilityConstant.h"
-#include "GolemPermeabilityKC.h"
-#include "GolemPermeabilityCubicLaw.h"
-#include "GolemHardeningConstant.h"
-#include "GolemHardeningCubic.h"
-#include "GolemHardeningExponential.h"
-#include "GolemHardeningPlasticSaturation.h"
-
 template <>
 InputParameters
 validParams<GolemApp>()
@@ -108,6 +39,13 @@ GolemApp::GolemApp(InputParameters parameters) : MooseApp(parameters)
 
 GolemApp::~GolemApp() {}
 
+static void
+associateSyntaxInner(Syntax & syntax, ActionFactory & /*action_factory*/)
+{
+  registerSyntax("EmptyAction", "BCs/GolemPressure");
+  registerSyntax("GolemPressureAction", "BCs/GolemPressure/*");
+}
+
 void
 GolemApp::registerApps()
 {
@@ -119,91 +57,5 @@ GolemApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
 {
   Registry::registerObjectsTo(f, {"GolemApp"});
   Registry::registerActionsTo(af, {"GolemApp"});
-}
-
-void
-GolemApp::registerObjects(Factory & factory)
-{
-  mooseDeprecated("use registerAll instead of registerObjects");
-  // Materials
-  registerMaterial(GMSMaterial);
-  registerMaterial(GolemMaterialBase);
-  registerMaterial(GolemMaterialH);
-  registerMaterial(GolemMaterialT);
-  registerMaterial(GolemMaterialTH);
-  registerMaterial(GolemMaterialMElastic);
-  registerMaterial(GolemMaterialMInelastic);
-  registerMaterial(GolemDruckerPrager);
-
-  // Kernels
-  registerKernel(GMSEnergyTimeDerivative);
-  registerKernel(GMSEnergyResidual);
-  registerKernel(GMSMassResidual);
-  registerKernel(GolemKernelTimeH);
-  registerKernel(GolemKernelTimeT);
-  registerKernel(GolemKernelH);
-  registerKernel(GolemKernelT);
-  registerKernel(GolemKernelTH);
-  registerKernel(GolemKernelM);
-  registerKernel(GolemKernelHPoroElastic);
-
-  // AuxKernels
-  registerAux(GolemDarcyVelocity);
-  registerAux(GolemFluidVelocity);
-  registerAux(GolemStress);
-  registerAux(GolemStrain);
-  registerAux(GolemThermalStress);
-  registerAux(GolemEqvInelasticStrain);
-
-  // DiracKernels
-  registerDiracKernel(GolemDiracKernelTH);
-
-  // BCs
-  registerBoundaryCondition(GolemConvectiveTHBC);
-  registerBoundaryCondition(GolemVelocityBC);
-  registerBoundaryCondition(GolemHeatFlowBC);
-  registerBoundaryCondition(GolemPressureBC);
-
-  // Controls
-  registerControl(GolemTimeControl);
-
-  // Functions
-  registerFunction(GolemFunctionBCFromFile);
-  registerFunction(GolemFunctionReadFile);
-
-  // UserObjects
-  registerUserObject(GolemScaling);
-  registerUserObject(GolemSUPG);
-  registerUserObject(GolemPropertyReadFile);
-  registerUserObject(GolemFluidDensityConstant);
-  registerUserObject(GolemFluidDensityLinear);
-  registerUserObject(GolemFluidDensityIAPWS);
-  registerUserObject(GolemFluidViscosityConstant);
-  registerUserObject(GolemFluidViscosityLinear);
-  registerUserObject(GolemFluidViscosityIAPWS);
-  registerUserObject(GolemPorosityConstant);
-  registerUserObject(GolemPorosityTHM);
-  registerUserObject(GolemPermeabilityConstant);
-  registerUserObject(GolemPermeabilityKC);
-  registerUserObject(GolemPermeabilityCubicLaw);
-  registerUserObject(GolemHardeningConstant);
-  registerUserObject(GolemHardeningCubic);
-  registerUserObject(GolemHardeningExponential);
-  registerUserObject(GolemHardeningPlasticSaturation);
-}
-
-void
-GolemApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
-{
-  mooseDeprecated("use registerAll instead of associateSyntax");
-  registerSyntax("EmptyAction", "BCs/GolemPressure");
-  registerSyntax("GolemPressureAction", "BCs/GolemPressure/*");
-
-  registerAction(GolemPressureAction, "add_bc");
-}
-
-extern "C" void
-GolemApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
-{
-  GolemApp::registerAll(f, af, s);
+  associateSyntaxInner(s, af);
 }
